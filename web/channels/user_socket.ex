77b073d84f0843ff1e6f3ec0968038fcbe6a7c59
Aaron Greenspan <afg419@gmail.com>
2016-04-10 16:51:53 -0600

    Implements socket

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule TwoChatty4me.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "rooms:*", TwoChatty4me.RoomChannel
+  channel "rooms:*", TwoChatty4me.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
